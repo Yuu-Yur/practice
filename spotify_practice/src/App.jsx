751c5e6b3148d28ae299f0e375eb3d12f11bdae4
Yuu-Yur <gkcjdqls@gmail.com>
2025-01-24 17:32:29 +0900

    first commit

---
+++ spotify_practice/src/App.jsx
@@ -0,0 +1,20 @@
+import "./App.css";
+import SpotifyLogin from "./components/SpotifyLogin";
+import { Route, Routes } from "react-router-dom";
+import Callback from "./components/Callback";
+import MainPage from "./pages/Mainpage";
+
+function App() {
+  return (
+    <>
+      <Routes>
+        <Route path="/" element={<MainPage />} />
+        <Route path="/:category" element={<MainPage />} />
+        <Route path="/login" element={<SpotifyLogin />} />
+        <Route path="/Callback" element={<Callback />} />
+      </Routes>
+    </>
+  );
+}
+
+export default App;
